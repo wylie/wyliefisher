8696aa37d545517fca1474552166ae2a797ce2b9
Wylie Fisher <me@wyliefisher.com>
2024-12-26 20:24:57 -0500

    adding a "work in progress" bit to the text about Santa Commands It

--- src/projects.js
+++ src/projects.js
@@ -7,7 +7,7 @@ const projects = [
   },
   {
     title: "Santa Commands It!",
-    description: "This is a playful React app where Santa has taken a commanding turn, granting (or denying) users' holiday wishes with a touch of humor and seasonal flair. Users submit their requests, and Santa, now a bit more 'powerful,' considers each one and delivers a dynamic, festive response. Enjoy making wishes, but beware—Santa's judgment may surprise you!",
+    description: "This work in progress is a playful React app where Santa has taken a commanding turn, granting (or denying) users' holiday wishes with a touch of humor and seasonal flair. Users submit their requests, and Santa, now a bit more 'powerful,' considers each one and delivers a dynamic, festive response. Enjoy making wishes, but beware—Santa's judgment may surprise you!",
     link: "https://wylie.github.io/SantaCommandsIt/"
   },
   {
