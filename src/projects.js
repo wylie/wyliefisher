06b26014f62ac6f83153160f1506c935624c6aa5
Wylie Fisher <me@wyliefisher.com>
2025-01-14 12:16:35 -0500

    add and rearrange projects

--- src/projects.js
+++ src/projects.js
@@ -6,19 +6,19 @@ const projects = [
     link: "https://wylie.github.io/ifeelblank/"
   },
   {
-    title: "Santa Commands It!",
-    description: "This work in progress is a playful React app where Santa has taken a commanding turn, granting (or denying) users' holiday wishes with a touch of humor and seasonal flair. Users submit their requests, and Santa, now a bit more 'powerful,' considers each one and delivers a dynamic, festive response. Enjoy making wishes, but beware—Santa's judgment may surprise you!",
-    link: "https://wylie.github.io/SantaCommandsIt/"
-  },
-  {
-    title: "Wampa Stompa",
-    description: "This lets you design your own Wampa Stompa mix image! The image is inspired by the Darth Punk video. Customize the visuals to capture the spirit of Wampa Stompa and create your unique image remix!",
-    link: "https://wylie.github.io/wampastompa/#make-a-new"
+    title: "Star Wars Timeline",
+    description: "As a self-proclaimed Star Wars nerd, I'm building a comprehensive repository of Star Wars media, including books, movies, TV shows, comics, and more. The goal is to create a centralized, complete, and up-to-date resource for fans of every level to explore the Star Wars universe. Whether you're a seasoned Jedi or a Padawan just starting your journey, this guide will help you navigate the galaxy far, far away.",
+    link: "https://wylie.github.io/starwars"
   },
   {
     title: "Maze Game",
     description: "This is a maze game developed in Lua in Pico-8. In this game, you navigate a little character through a dark maze, trying to find the exit. The maze is filled with twists and turns, and you'll need to use your wits and memory to avoid dead ends and find your way out. Good luck, and happy maze-solving!",
     link: "https://wylie.github.io/pico-8/"
+  },
+  {
+    title: "Santa Commands It!",
+    description: "This work in progress is a playful React app where Santa has taken a commanding turn, granting (or denying) users' holiday wishes with a touch of humor and seasonal flair. Users submit their requests, and Santa, now a bit more 'powerful,' considers each one and delivers a dynamic, festive response. Enjoy making wishes, but beware—Santa's judgment may surprise you!",
+    link: "https://wylie.github.io/SantaCommandsIt/"
   }
 ];
 
