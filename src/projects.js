7bfe03cd18ce6a398d75d358e67b90ae8b0bb7cb
Wylie Fisher <me@wyliefisher.com>
2025-01-17 15:40:42 -0500

    cleaning up for mobile

--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,7 @@ const projects = [
     link: "https://wylie.github.io/ifeelblank/"
   },
   {
-    title: "Star Wars Timeline",
+    title: "Nerds (Star Wars) Timeline",
     description: "As a self-proclaimed Star Wars nerd, I'm building a comprehensive repository of Star Wars media, including books, movies, TV shows, comics, and more. The goal is to create a centralized, complete, and up-to-date resource for fans of every level to explore the Star Wars universe. Whether you're a seasoned Jedi or a Padawan just starting your journey, this guide will help you navigate the galaxy far, far away.",
     link: "https://wylie.github.io/starwars"
   },
