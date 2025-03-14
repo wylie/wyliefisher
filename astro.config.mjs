a8c02302a64196d241d36fc0f2cdb01e964b33bc
Wylie Fisher <me@wyliefisher.com>
2025-03-13 20:46:05 -0400

    testing out the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wyliefisher.com',
   integrations: [mdx(), sitemap(), react({
     config: {
       forward: ["dataLayer.push"],
