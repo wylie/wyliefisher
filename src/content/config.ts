5b8223f4064c6e04574cb565aec586332cf5d586
Wylie Fisher <me@wyliefisher.com>
2026-03-01 20:51:19 -0500

    Refactor blog previews to content excerpts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,12 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   schema: ({ image }) => z.object({
-    title: z.string(),
 		pubDate: z
 			.string()
 			.or(z.date())
 			.transform((val) => new Date(val)),
 		heroImage: image(),
-		tags: z.array(z.string()),
-		snippet: z.string().optional()
+		tags: z.array(z.string())
   }),
 });
 
